78a079b7c2dff86431230e8f3f8a435f5138a7ae
sundongmin <sundmdm@gmail.com>
2019-09-02 15:09:17 +0800

    init

---
+++ learn-spring/src/main/java/com/github/IndexService.java
@@ -0,0 +1,7 @@
+package com.github;
+
+import org.springframework.stereotype.Component;
+
+@Component("index")
+public class IndexService {
+}
---
+++ learn-spring/src/main/java/com/github/Test.java
@@ -0,0 +1,13 @@
+package com.github;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		IndexService service = context.getBean("index", IndexService.class);
+		System.out.println(service);
+
+	}
+}
