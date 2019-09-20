e0bb6b4143e5658571f0c8c10280f90046a8c314
sundongmin <sundmdm@gmail.com>
2019-09-20 17:58:27 +0800

    添加一些注释

--- learn-spring/src/main/java/com/github/config/AppConfig.java
+++ learn-spring/src/main/java/com/github/config/AppConfig.java
@@ -1,13 +1,15 @@
 package com.github.config;
 
+import com.github.context.MyImportBeanDefinitionRegistrar;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan("com.github")
 @EnableAspectJAutoProxy
-//@Import(MyImportBeanDefinitionRegistrar.class)
+@Import(MyImportBeanDefinitionRegistrar.class)
 public class AppConfig {
 
 }
